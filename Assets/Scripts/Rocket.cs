ab70a8c683b26034c0ad43875d299055ea3f4d64
ViacheslavMiroshnykov <viacheslav.miroshnykov@gmail.com>
2024-10-21 00:16:51 +0300

    Add sound rocket

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSourse;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSourse = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,6 +25,14 @@ public class Rocket : MonoBehaviour
         if(Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (audioSourse.isPlaying == false)
+            {
+                audioSourse.Play();
+            }
+        }
+        else 
+        {
+            audioSourse.Pause();
         }
         if(Input.GetKey(KeyCode.A))
         {
