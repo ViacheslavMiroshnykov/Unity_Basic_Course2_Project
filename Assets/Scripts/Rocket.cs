ebf1966813c6602d668a04088e592fcdcd9a2b35
ViacheslavMiroshnykov <viacheslav.miroshnykov@gmail.com>
2024-10-20 23:30:26 +0300

    Created a turbine

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            print("Зажали А");
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            print("Зажали В");
+        }
+    }
+}
